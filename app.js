2101874ec8651f89bbd0c0bb79dd7fef6220b599
Joe Rezendes <rezenbiz@gmail.com>
2017-12-27 11:04:32 -0800

    fixed heroku stuff

--- app.js
+++ app.js
@@ -103,6 +103,6 @@ app.delete('/reviews/:id', function (req, res) {
   })
 })
 
-app.listen(3000, () => {
+app.listen(3000 || process.env.PORT, () => {
   console.log('App listening on port 3000!');
 })
