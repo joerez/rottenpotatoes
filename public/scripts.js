188e49f1936ea8770f8615c30332c8f861cdf083
Joe Rezendes <rezenbiz@gmail.com>
2017-12-27 15:06:26 -0800

    Finished Product

---
+++ public/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function()
+{
+
+
+
+
+
+
+  $(".portmation").hover(
+      function () {
+        $(this).addClass('swing infinite');
+      },
+      function () {
+        $(this).removeClass("swing infinite");
+      }
+    );
+
+
+
+});
