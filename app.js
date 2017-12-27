1f248b03eda6f396bb18229dd666fc9cc26addc4
Joe Rezendes <rezenbiz@gmail.com>
2017-12-26 18:03:46 -0800

    Working basic backend with one field

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var exphbs = require('express-handlebars');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+
+const Review = mongoose.model('Review', {
+  title: String
+});
+
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -10,17 +32,26 @@ app.set('view engine', 'handlebars');
 })*/
 
 //mock array of projects
-let reviews = [
+
+/*let reviews = [
   {title: "Star Wars"},
   {title: "Harry Potter"}
 ];
-
+*/
 
 //index
 app.get('/', (req, res) => {
-  res.render('reviews-index', {reviews: reviews});
+  Review.find().then((reviews) => {
+    res.render('reviews-index', {reviews: reviews});
+  }).catch((err) => {
+    console.log(err);
+  })
 })
 
+// NEW
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
